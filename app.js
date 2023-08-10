91e499a8cb7d7c8d6155e9a6c47ce043cd94b8b8
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-10 18:49:22 -0400

    created the draw function

--- app.js
+++ app.js
@@ -35,4 +35,23 @@ document.addEventListener('DOMContentLoaded', () => {
     [1, width + 1, 2 * width + 1, 3 * width + 1],
     [width, width + 1, width + 2, width + 3],
   ];
+
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
+
+  let currentPosition = 4;
+  let current = theTetrominoes[0][0];
+
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add('tetromino');
+    });
+  }
+
+  draw();
 });
