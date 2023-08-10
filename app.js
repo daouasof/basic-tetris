f339793ae801d6f02e418501931aff31ddbc731e
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-10 17:52:03 -0400

    created the lTetromino

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10;
   const scoreDisplay = document.querySelector('#score');
   const startBtn = document.querySelector('#start-button');
+
+  const lTetromino = [
+    [1, width + 1, 2 * width + 1, 2],
+    [width, width + 1, width + 2, 2 * width + 2],
+    [1, width + 1, 2 * width + 1, 2 * width],
+    [width, 2 * width, 2 * width + 1, 2 * width + 2],
+  ];
 });
