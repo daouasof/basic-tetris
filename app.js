8389706adb928c922bd42619f7702bfa71b9d0b7
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-08-10 17:25:07 -0400

    added querySelectors

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {});
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const width = 10;
+  const scoreDisplay = document.querySelector('#score');
+  const startBtn = document.querySelector('#start-button');
+});
